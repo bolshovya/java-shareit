ccd243ec710eead4c8ac7db4373835ab75f97427
bolshovya <bolshov.yv@yandex.ru>
2023-09-03 01:27:43 +0300

    test: добавил UserServiceIntegratiomTest

--- src/test/java/ru/practicum/shareit/user/service/UserServiceIntegratiomTest.java
+++ src/test/java/ru/practicum/shareit/user/service/UserServiceIntegratiomTest.java
@@ -4,8 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.shareit.user.User;
 import ru.practicum.shareit.user.dto.UserDto;
@@ -16,8 +14,6 @@ import javax.persistence.TypedQuery;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.notNullValue;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 
 @Transactional
