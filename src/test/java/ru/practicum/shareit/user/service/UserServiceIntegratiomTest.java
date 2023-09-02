e3794bb1b5ea6d81f6f7b9981210508806031598
bolshovya <bolshov.yv@yandex.ru>
2023-09-03 01:25:41 +0300

    test: добавил UserServiceIntegratiomTest

---
+++ src/test/java/ru/practicum/shareit/user/service/UserServiceIntegratiomTest.java
@@ -0,0 +1,54 @@
+package ru.practicum.shareit.user.service;
+
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import org.springframework.transaction.annotation.Transactional;
+import ru.practicum.shareit.user.User;
+import ru.practicum.shareit.user.dto.UserDto;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@Transactional
+@SpringBootTest(
+        properties = "db.name=test",
+        webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
+class UserServiceIntegratiomTest {
+
+    private final EntityManager em;
+    private final UserService service;
+
+    @Test
+    void saveUser() {
+        UserDto userDto = makeUserDto("User1", "user1@user.com");
+        service.create(userDto);
+
+        TypedQuery<User> query = em.createQuery("SELECT u FROM User u WHERE u.email = :email", User.class);
+        User user = query
+                .setParameter("email", userDto.getEmail())
+                .getSingleResult();
+
+        assertThat(user.getId(), notNullValue());
+        assertThat(user.getName(), equalTo(userDto.getName()));
+        assertThat(user.getEmail(), equalTo(userDto.getEmail()));
+    }
+
+    private UserDto makeUserDto(String name, String email) {
+        UserDto dto = new UserDto();
+        dto.setName(name);
+        dto.setEmail(email);
+        return dto;
+    }
+}
